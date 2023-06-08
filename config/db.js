9ae2a999e1874a3d685dd23237a5f7bc8ea96e4f
Jaliel <kangjaliel1998@gmail.com>
2023-06-08 17:34:55 +0700

    new env

--- config/db.js
+++ config/db.js
@@ -1,8 +1,16 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("darsun_invitation", "admin", "DarsunNov17", {
-	host: "localhost",
+// const db = new Sequelize("darsun_undangan", "root", "", {
+// 	host: "localhost",
+// 	dialect: "mysql",
+// });
+
+const db = new Sequelize({
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DATABASE,
+	port: process.env.DB_PORT,
+	host: process.env.DB_HOST,
 	dialect: "mysql",
 });
-
 export default db;
